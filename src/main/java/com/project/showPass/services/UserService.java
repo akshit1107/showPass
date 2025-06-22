b84485b6c79f7426f6fbb9baec7f4c3c80afda28
Akshit Tyagi <this.akshit.11@gmail.com>
2025-06-23 05:12:39 +0530

    added mailer

--- src/main/java/com/project/showPass/services/UserService.java
+++ src/main/java/com/project/showPass/services/UserService.java
@@ -1,6 +1,8 @@
 package com.project.showPass.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.project.showPass.models.User;
@@ -12,8 +14,26 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     public String addUser(User user) {
         User userObj = userRepository.save(user);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("Welcome to ShowPass");
+        message.setFrom("this.akshit.11@gmail.com");
+        message.setTo(userObj.getEmailId());
+
+        String body = "Dear " + userObj.getUserName() + ",\n\n" +
+                "Welcome to ShowPass! We are thrilled to have you on board.\n" +
+                "Your user ID is: " + userObj.getUserId() + "\n\n" +
+                "Best regards,\n" +
+                "ShowPass Team";
+
+        message.setText(body);
+        javaMailSender.send(message);
+
         return "User added successfully with ID: " + userObj.getUserId();
     }
     
